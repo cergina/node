1c1282a5d32ffc0fc79de61b1a042d7714d822d0
cergina <mcerget@gmail.com>
2021-09-18 15:13:50 +0200

    Tutorial finished

--- WeatherApp/public/script.js
+++ WeatherApp/public/script.js
@@ -38,10 +38,25 @@ function setWeatherData(data, place) {
     temperatureElement.textContent = data.current.temperature
     precipitationElement.textContent = `${data.current.precip * 100}%`
 
-    var ctx=iconCanvas.getContext("2d");
+    
     var img=new Image();
-    img.onload = function(){
-          ctx.drawImage(img,0,0);
-    };
     img.src=data.current.weather_icons[0];
+
+    img.onload = function(){
+        var ctx=iconCanvas.getContext("2d");
+        var wrh = img.width / img.height;
+        var newWidth = iconCanvas.width;
+        var newHeight = newWidth / wrh;
+
+        if (newHeight > iconCanvas.height) {
+            newHeight = iconCanvas.height;
+            newWidth = newHeight * wrh;
+        }
+
+        var xOffset = newWidth < iconCanvas.width ? ((iconCanvas.width - newWidth) / 2) : 0;
+        var yOffset = newHeight < iconCanvas.height ? ((iconCanvas.height - newHeight) / 2) : 0;
+
+        ctx.drawImage(img,xOffset,yOffset, newWidth, newHeight);
+    };
+    
 }
